e4bff180a406e5cac157480613be1145a836dcea
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-06 23:15:02 +0530

    view

--- src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
+++ src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.DataTruncation;
+
 @RestController
 public class PatientController {
 
     @PostMapping("/add")
     public String AddPatient(){
         return "patient added";
+    }
+
+    @GetMapping("/view")
+    public String ViewPatient(){
+        return " view patient";
     }
 
 
