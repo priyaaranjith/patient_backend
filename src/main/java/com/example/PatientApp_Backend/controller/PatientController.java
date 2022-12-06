5abd51b3d67ba466c84b436ecc9fc89faa2d2e7c
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-06 23:08:12 +0530

    first

---
+++ src/main/java/com/example/PatientApp_Backend/controller/PatientController.java
@@ -0,0 +1,18 @@
+package com.example.PatientApp_Backend.controller;
+
+
+import jakarta.persistence.GeneratedValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientController {
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/example/PatientApp_Backend/PatientAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PatientApp_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
